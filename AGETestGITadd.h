fc239bac6cc4873d9fa5447e63b1e2f83c0134c8
Andrew Easton <andrewgeraldeaston@Andrews-MacBook-Air.local>
2014-08-21 12:05:34 +0100

    added a test class to try GIT out

---
+++ AGETestGITadd.h
@@ -0,0 +1,13 @@
+//
+//  AGETestGITadd.h
+//  AGEGitTest
+//
+//  Created by Andrew Easton on 21/08/2014.
+//  Copyright (c) 2014 IOS7Course-age. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGETestGITadd : NSObject
+
+@end
---
+++ AGETestGITadd.m
@@ -0,0 +1,13 @@
+//
+//  AGETestGITadd.m
+//  AGEGitTest
+//
+//  Created by Andrew Easton on 21/08/2014.
+//  Copyright (c) 2014 IOS7Course-age. All rights reserved.
+//
+
+#import "AGETestGITadd.h"
+
+@implementation AGETestGITadd
+
+@end
